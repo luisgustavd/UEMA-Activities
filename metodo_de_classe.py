4918ccb1d29ea058b11499a2f288a3c18428e1f9
Luis Gustavo Diniz Pereira <79110390+luisgustavd@users.noreply.github.com>
2022-04-12 09:17:31 -0300

    Update metodo_de_classe.py

--- metodo_de_classe.py
+++ metodo_de_classe.py
@@ -1,3 +1,5 @@
+from random import randint #MÓDULO PYTHON DE NÚMEROS ALEATÓRIOS 
+
 class Pessoa:
     ano_atual= 2022 #Atributo de classe
     def __init__(self,nome,idade):
@@ -11,8 +13,14 @@ class Pessoa:
     def por_ano_de_nascimento(cls,nome,ano_nascimento): # MÉTODO DE CLASSE QUE CRIA OBJETOS
         idade = cls.ano_atual - ano_nascimento
         return cls(nome, idade)
+    
+    @staticmethod #DECORADOR
+    def get_id(): # MÉTODO DE CLASSE ESTÁTICO
+        rand = randint(0,10)
+        return rand 
 
 p1 = Pessoa.por_ano_de_nascimento("Luis Gustavo",2002) # PASSANDO PARÂMETROS
 print(p1)
 print(p1.nome, p1.idade) # IMPRIMINDO
 p1.get_data_de_nascimento()
+print(Pessoa.gerar_id()) # GERANDO UM NÚMERO ID ALEATÓRIO DE 0 A 10 
